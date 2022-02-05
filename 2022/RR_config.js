95c06626bf0065e8c1a8eada280fa7265cc24faf
Space Cookies <1868@spacecookies.org>
2022-02-05 12:47:08 -0800

    removed connection br

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -207,7 +207,7 @@ var config_data = `
         "choices":{
           "w":"Yes<br>",
           "s":"Slight<br>",
-          "z":"None<br>",
+          "z":"None",
         },
         "defaultValue":"z"
       },
